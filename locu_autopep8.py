ba08b147acf26e8f7516170cde7675f61e7a538c
Jason Ansel <jansel@locu.com>
2013-06-06 02:59:46 +0000

    Bugfixes in locu wrapper

--- locu_autopep8.py
+++ locu_autopep8.py
@@ -9,6 +9,7 @@ except ImportError:
     from io import StringIO
 
 import autopep8
+from collections import defaultdict
 
 
 def fix_l99901(source):
@@ -16,29 +17,39 @@ def fix_l99901(source):
     Replace "x" strings with 'x' strings
     Replace '''y''' strings with "\""y"\""
     """
-    lines = StringIO(source).readlines()
-    for i, line in enumerate(lines):
-        try:
-            g = tokenize.generate_tokens(StringIO(line).readline)
-            for toknum, val, _, _, _ in g:
-                if toknum == tokenize.STRING:
-                    newval = None
+    replacements = defaultdict(int)
+    try:
+        g = tokenize.generate_tokens(StringIO(source).readline)
+        for toknum, val, _, _, _ in g:
+            if toknum == tokenize.STRING:
+                newval = None
 
-                    m = re.match(r'^"([^"\'\\]*)"$', val)
-                    if m:
-                        newval = u"'{0}'".format(m.group(1))
+                m = re.match(r'^"([^"\'\\]*)"$', val)
+                if m:
+                    newval = u"'{0}'".format(m.group(1))
 
-                    m = re.match(r"^'''([^\"'\\]*)'''$", val)
-                    if m:
-                        newval = u'"""{0}"""'.format(m.group(1))
+                m = re.match(r"^'''([^\"'\\]*)'''$", val)
+                if m:
+                    newval = u'"""{0}"""'.format(m.group(1))
 
-                    if (newval and
-                            line.replace(val, newval, 1) == line.replace(val, newval, 2)):
-                        lines[i] = line.replace(val, newval, 1)
+                m = re.match(r'^"(\\[a-z])"$', val)
+                if m:
+                    newval = u"'{0}'".format(m.group(1))
 
-        except tokenize.TokenError:
-            pass
-    return ''.join(lines)
+                if newval:
+                    replacements[(val, newval)] += 1
+    except tokenize.TokenError:
+        pass
+    except IndentationError:
+        pass
+
+    for (old, new), count in replacements.items():
+        tmp = source.replace(old, new, count)
+        # check to make sure number of replacements is correct
+        if tmp.replace(old, new) == tmp:
+            source = tmp
+
+    return source
 
 
 def fix_l99902(source):
@@ -50,7 +61,7 @@ def fix_l99902(source):
         if ('coding' not in lines[0].lower() and
                 'coding' not in lines[1].lower()):
             pos = 1 if lines[0][:2] == "#!" else 0
-            lines.insert(pos, '# coding: utf-8')
+            lines.insert(pos, '# coding: utf-8\n')
     return ''.join(lines)
 
 autopep8.fix_l99901 = fix_l99901
